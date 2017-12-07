997f68911a609404eb8383e0bcbbf0b233f42126
mgarcia707 <mijogarc@ucsc.edu>
2017-12-06 21:08:16 -0800

    design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import gear from './gear.svg';
 import michael from './michael.jpg';
+import solarsys from './solarsys.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -9,11 +11,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={gear} className="App-logo" alt="gear" />
-          <h1 className="App-title">Let's build something together</h1>
+          <h1 className="App-title">Weclome to the world of Michael</h1>
+          <img src={solarsys} className="App-logo" alt="solarsys" />
         </header>
         <p className="App-intro">
-          <img src={michael} className="Image" alt="michael" />
+           <img src={michael} className="Image" alt="michael" />
         </p>
       </div>
     );
