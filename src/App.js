732f5c92be429736a47d43b6f0bb521ad9a09116
mgarcia707 <mijogarc@ucsc.edu>
2017-12-20 01:29:19 -0800

    new desgin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import gear from './gear.svg';
 import michael from './michael.jpg';
-import solarsys from './solarsys.svg';
+import earth from './earth.svg';
+import bulb from './bulb.svg';
+import binary from './binary.svg';
+import programming from './programming.svg';
+import penpaper from './penpaper.svg';
 
 import './App.css';
 
@@ -11,8 +15,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Weclome to the world of Michael</h1>
-          <img src={solarsys} className="App-logo" alt="solarsys" />
+          <h1 className="App-title">"The important thing is to not stop questioning;
+            curiosity has its own reason for exisiting." - Albert Einstein</h1>
+          <img src={penpaper} className="App-logo" alt="penpaper" />
+          <img src={bulb} className="App-logo" alt="bulb" />
+          <img src={gear} className="App-logo-spin" alt="gear" />
+          <img src={binary} className="App-logo" alt="binary" />
+          <img src={programming} className="App-logo" alt="programming" />
         </header>
         <p className="App-intro">
            <img src={michael} className="Image" alt="michael" />
