c0895179de2bb74846a959f2c9f248e23cd2d3b0
mgarcia707 <mijogarc@ucsc.edu>
2017-12-21 21:19:27 -0800

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import gear from './gear.svg';
 import michael from './michael.jpg';
-import earth from './earth.svg';
-import bulb from './bulb.svg';
-import binary from './binary.svg';
 import programming from './programming.svg';
-import penpaper from './penpaper.svg';
 
 import './App.css';
 
@@ -15,13 +11,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">"The important thing is to not stop questioning;
-            curiosity has its own reason for exisiting." - Albert Einstein</h1>
-          <img src={penpaper} className="App-logo" alt="penpaper" />
-          <img src={bulb} className="App-logo" alt="bulb" />
-          <img src={gear} className="App-logo-spin" alt="gear" />
-          <img src={binary} className="App-logo" alt="binary" />
-          <img src={programming} className="App-logo" alt="programming" />
+        <h1> Welcome! </h1>
+        <h1 className="App-title">I am currently a software developer located
+          in Los Gatos, California. </h1>
+        <h1 className="App-title">  I received my Bachelor's in Computer
+         Science from the University of California, Santa Cruz. </h1>
         </header>
         <p className="App-intro">
            <img src={michael} className="Image" alt="michael" />
